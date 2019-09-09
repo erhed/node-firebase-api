d9fc4060fd389b60f10bebba9d4ad16035694174
Adam Lang <Adam.Lang-1@hotmail.com>
2019-09-09 11:30:38 +0200

    firebase key

--- index.js
+++ index.js
@@ -2,3 +2,37 @@ let express = require("express");
 let app = express();
 let async  = require('express-async-await');
 let fetch = require('node-fetch');
+
+
+
+
+
+
+
+
+
+
+// HAHDAUDFHW
+
+
+
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.6.0/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+    //  https://firebase.google.com/docs/web/setup#config-web-app -->
+  /* // Your web app's Firebase configuration */
+  {/* // Initialize Firebase */}
+
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCMxK2gLdb7KFtBuAc_T1OAAadutg-YQY0",
+    authDomain: "currency-api-3c271.firebaseapp.com",
+    databaseURL: "https://currency-api-3c271.firebaseio.com",
+    projectId: "currency-api-3c271",
+    storageBucket: "",
+    messagingSenderId: "996034716897",
+    appId: "1:996034716897:web:b86a3fabe2303b770883a0"
+  };
+  firebase.initializeApp(firebaseConfig);
