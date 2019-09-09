6f26fb55eab5a6c501977ac36ee38f6c8ff23933
erhed <erik.hedrenius@gmail.com>
2019-09-09 15:32:45 +0200

    Merged

--- index.js
+++ index.js
@@ -2,32 +2,39 @@ let express = require("express");
 let app = express();
 let async = require('express-async-await');
 let fetch = require('node-fetch');
+var firebase = require('firebase');
 
+// Firebase config
 
+var firebaseConfig = {
+    apiKey: "AIzaSyCMxK2gLdb7KFtBuAc_T1OAAadutg-YQY0",
+    authDomain: "currency-api-3c271.firebaseapp.com",
+    databaseURL: "https://currency-api-3c271.firebaseio.com",
+    projectId: "currency-api-3c271",
+    storageBucket: "",
+    messagingSenderId: "996034716897",
+    appId: "1:996034716897:web:b86a3fabe2303b770883a0"
+};
 
- function putData(collection, currency1, currency2) {
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const collection = "CurrencyPrices";
+
+// Functions
+
+function putData(collection, currency1, currency2) {
 
     db.collection(collection).doc(`${currency1}${currency2}`).set({
 
-      
     })
-    .then(function() {
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
-    test.firestore.js
-    
-   
-  
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
+
 }
-
- 
-
-var firebase = require('firebase');
-var app = firebase.initializeApp();
-
 
 function getDataFromFirebase(currency1, currency2) {
 
@@ -44,58 +51,26 @@ function getDataFromFirebase(currency1, currency2) {
         });
 }
 
-app.listen(3000, function () {
-    console.log('Running on port 3000');
-})
-
-db.collection(collection).doc(`${currency1}${currency2}`);
-
-var firebaseConfig = {
-    apiKey: "AIzaSyCMxK2gLdb7KFtBuAc_T1OAAadutg-YQY0",
-    authDomain: "currency-api-3c271.firebaseapp.com",
-    databaseURL: "https://currency-api-3c271.firebaseio.com",
-    projectId: "currency-api-3c271",
-    storageBucket: "",
-    messagingSenderId: "996034716897",
-    appId: "1:996034716897:web:b86a3fabe2303b770883a0"
-};
-firebase.initializeApp(firebaseConfig);
-
-
-  // GET
-// ex: USD/SEK, priset för en dollar i kronor
-// - kolla om datan redan finns i FIREBASE (kolla datum?)
-// - finns den där skickar vi den till användaren
-// - finns den inte ska vi göra en API call till externa APIn och skicka den datan till användaren OCH till firebase
-
-
-
-
-
-function deleteData(currency1, currency2){
-    db.collection(collection).doc(`${currency1}${currency2}`).delete().then(function() {
+function deleteData(currency1, currency2) {
+    db.collection(collection).doc(`${currency1}${currency2}`).delete().then(function () {
         console.log(`Document ${docToDelete} successfully deleted!`);
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.error("Error removing document: ", error);
     })
 }
 
-
-
-function postDataToFirebase(currency1, currency2, json){
+function postDataToFirebase(currency1, currency2, json) {
 
     // Add a new document in collection "cities"
-db.collection(collection).doc(`${currency1}${currency2}`).set({
-    // name: "Los Angeles",
-    // state: "CA",
-    // country: "USA"
-})
-.then(function() {
-    console.log("Document successfully written!");
-})
-.catch(function(error) {
-    console.error("Error writing document: ", error);
-});
+    db.collection(collection).doc(`${currency1}${currency2}`).set({
+        json: json
+    })
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
 
 }
 
@@ -106,10 +81,23 @@ async function getDataFromApi(currency1, currency2) {
     return fetch(url, { "method": "GET", }).then((result) => {
         return result.json();
     }).then(resultJson => {
+        console.log(resultJson);
         return resultJson;
     })
-    .catch((err) => {
-        console.log(err)
-    });
+        .catch((err) => {
+            console.log(err)
+        });
 }
 
+app.post('/POST', async function(req, res) {
+    let json = await getDataFromApi("USD", "JPY");
+    console.log(json);
+
+    postDataToFirebase("USD","JPY", JSON.stringify(json));
+
+    res.status(200).send("lol");
+});
+
+app.listen(3000, function () {
+    console.log('Running on port 3000');
+})
