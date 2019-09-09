ac922aed0f1d14c8ea26f5bc416352953ccb9c3c
Adriana Larsson <adrianalarsson@Adrianas-MacBook-Air.local>
2019-09-09 14:21:03 +0200

    did put function

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ let app = express();
 let async  = require('express-async-await');
 let fetch = require('node-fetch');
 
+
+ function putData(currency1, currency2 ) {
+    var url = `https://api.exchangeratesapi.io/latest?base=${currency1}&symbols=${currency2}`;
+   
+    fetch(url, { "method": "PUT", }).then((result) => {
+        console.log(result.body)
+    }).catch((err) => {
+        console.log(err)
+    });
+    
+
+}
+
+ 
+ app.listen(3000, function(){
+
+    console.log("RUNNING");
+    
+});
