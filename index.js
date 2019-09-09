5d7020a697b954692a5864479dfba4b46f8c8cb0
Philip Eriksson <phille.e.eriksson@gmail.com>
2019-09-09 14:54:35 +0200

    Uppdaterat parametrar

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let app = express();
 let async  = require('express-async-await');
 let fetch = require('node-fetch');
 
-function deleteData(docToDelete){
-    db.collection(collection).doc(docToDelete).delete().then(function() {
+function deleteData(docToDelete, currency1, currency2){
+    db.collection(collection).doc(`${currency1}${currency2}`).delete().then(function() {
         console.log(`Document ${docToDelete} successfully deleted!`);
     }).catch(function(error) {
         console.error("Error removing document: ", error);
