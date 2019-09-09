424a894083da742fea1b621fef53c04459401d51
Philip Eriksson <phille.e.eriksson@gmail.com>
2019-09-09 13:32:43 +0200

    Metod för att ta bort data från Firebase

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-//Phille was here
 let express = require("express");
 let app = express();
 let async  = require('express-async-await');
 let fetch = require('node-fetch');
+
+function deleteData(docToDelete){
+    db.collection(collectionName).doc(docToDelete).delete().then(function() {
+        console.log(`Document ${docToDelete} successfully deleted!`);
+    }).catch(function(error) {
+        console.error("Error removing document: ", error);
+    })
+}
