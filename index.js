657a7d5d323cb22339faf9e9ca691c73f0f01ea8
David Hjorth <david.hjorth@hotmail.com>
2019-09-09 11:32:48 +0200

    currency api working

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 let express = require("express");
 let app = express();
-let async  = require('express-async-await');
+let async = require('express-async-await');
 let fetch = require('node-fetch');
+
+function getDataFromApi(currency1, currency2) {
+
+    var url = `https://api.exchangeratesapi.io/latest?base=${currency1}&symbols=${currency2}`;
+
+    fetch(url, { "method": "GET", }).then((result) => {
+        console.log(result)
+    }).catch((err) => {
+        console.log(err)
+    });
+}
+
+getDataFromApi("USD", "SEK");
+
+
+app.get('/', function (req, res) {
+    return res.send('hejhej');
+});
