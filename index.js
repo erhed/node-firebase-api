bc664c426bc46c2d4aea614d3d286d534b9d7641
Philip Eriksson <phille.e.eriksson@gmail.com>
2019-09-09 14:51:15 +0200

    Ändrat namn på collection

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let async  = require('express-async-await');
 let fetch = require('node-fetch');
 
 function deleteData(docToDelete){
-    db.collection(collectionName).doc(docToDelete).delete().then(function() {
+    db.collection(collection).doc(docToDelete).delete().then(function() {
         console.log(`Document ${docToDelete} successfully deleted!`);
     }).catch(function(error) {
         console.error("Error removing document: ", error);
